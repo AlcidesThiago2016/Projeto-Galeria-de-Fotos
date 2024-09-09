2b104923883ec30fdd48993dbe16cde31360b4c6
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2024-09-09 16:27:38 -0300

    Update main.js implemented JQuery

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
 $(document).ready(function(){
-    console.log ($('header button'));
+    $('header button').click(function(){
+        $('form').slideDown();
+    })
+
+    $('#btn-cancel').click(function(){
+        $('form').slideUp();
+    })
+
+    $('form').on('submit', function(e){
+        e.preventDefault()
+        const endNewImg = $('#endereco-img-new').val();
+        const newItem = $('<li></li>');
+        $(`<img src="${endNewImg}" />`).appendTo(newItem);
+        $(`
+            <div class="overlay-img-link">
+                <a href="${endNewImg}" target="_black" title="Ver imagem em tamanho real">
+                    Ver imagem em tamanho real
+                </a>
+            </div>
+        `).appendTo(newItem);
+        $(newItem).appendTo('ul');
+        $(newItem).fadeIn(600);
+        $('#endereco-img-new').val('')
+    })
 })
